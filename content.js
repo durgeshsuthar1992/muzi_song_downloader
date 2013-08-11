b73a0631ba5801d7f9a9d266a6b3595e6a5fad7b
Durgesh suthar <durgesh.30@rediffmail.com>
2013-08-11 17:13:44 +0530

    collect currently playing song data

--- content.js
+++ content.js
@@ -1,7 +1,16 @@
 function get_mid(){
-	
 	var mid= document.getElementById('np_track').getAttribute('mid');
 	return mid;
 }
 var mid = get_mid();
 console.log(mid);
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange=function(){
+	if(xhr.readyState==4 && xhr.status==200)
+		var data=xhr.responseText;
+	else
+		console.log(xhr.readyState);
+}
+xhr.open("GET","https://sdslabs.co.in/muzi/ajax/track/?id="+mid,true);
+xhr.send();
+console.log(data);
